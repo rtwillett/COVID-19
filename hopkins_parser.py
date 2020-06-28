4eb8cebbe977634e2fb086d1874bd1640d9f55f4
Ryan Willett <ryanwillett@ryans-mbp.home>
2020-06-28 19:09:03 -0400

    Data update

--- hopkins_parser.py
+++ hopkins_parser.py
@@ -4,7 +4,7 @@ import copy
 
 def ts_record_parse(df, var_title = "Value"):
     '''
-    Takes in the column version of data from the JHU dataset and makes it long form in tidy format.
+
     '''
 
     ts_cols = df.columns[4:] # Collect the names of all the columns named as a date
@@ -14,7 +14,44 @@ def ts_record_parse(df, var_title = "Value"):
     ts_long.columns = ["Province", "Country", "Lat", "Lon", "Date", var_title]
     return(ts_long)
 
-# Summarizing by country
+def hongkong_isolate(df):
+    '''
+    Extracts the data for Hong Kong from the JHU data and creates a matching df format to the others used in this script
+    '''
+
+    hongkong_df = df.loc[df.Province == "Hong Kong"].copy()
+    hongkong_df['Country'] = "Hong Kong"
+    hongkong_df['Lat'] = 22.3193
+    hongkong_df['Lon'] = 114.1694
+    hongkong_df['Region'] = "Asia"
+
+    hongkong_out = hongkong_df[['Country', 'Date', 'Region', 'Lat', 'Lon', 'Confirmed']].copy()
+    hongkong_out['tooltip_confirmed'] = hongkong_out.apply(lambda x : x.Country + ": " + str(x.Confirmed), axis=1)
+
+    return(hongkong_out)
+
+def hongkong_extract(df_countryagg, df_raw):
+    '''
+    Gets the data for Hong Kong using he kongkong_isolate script, subtracts the HK numbers from China and updates the original dataframe with the new HK and China numbers.
+    '''
+
+    hk = hongkong_isolate(df_raw)
+    hongkong_series = hk.Confirmed.to_numpy()
+
+    china_slice = df_countryagg.loc[df_countryagg.Country == "China"].copy()
+    china_confirmed = china_slice.Confirmed.to_numpy() # Extracting Confirmed column as a vector
+
+    # Vectorized subtraction of the Hong Kong numbers from China numbers
+    china_newConfirmed = np.subtract(china_confirmed, hongkong_series)
+
+    # Extracting data from dataframe for all countries except China
+    not_china = df_countryagg.loc[df_countryagg.Country != "China"]
+    china_slice["Confirmed"] = china_newConfirmed.tolist()
+
+    df_out = pd.concat([not_china, china_slice, hk])
+
+    return(df_out)
+
 def regional_summarize(df, var, file_name):
     '''
 
@@ -32,6 +69,8 @@ def regional_summarize(df, var, file_name):
     data_tsSum_sort = data_tsSum_sort.reset_index()
 
     data_tsSum_sort[varname] = data_tsSum_sort.apply(lambda x : x.Country + ": " + str(x[var]), axis=1)
+#     data_tsSum_sort['tooltip_confirmed'] = data_tsSum_sort.apply(lambda x : x.Country + ": " + str(x.Confirmed), axis=1)
+#     data_tsSum_sort['tooltip_deaths'] = df_out['tooltip_deaths'] = df_out.apply(lambda x : x.state + ": " + str(x.Deaths), axis=1)
 
     data_tsSum_sort.to_feather(fileout)
 
@@ -73,10 +112,15 @@ def calculate_rate(df, var):
     return (df_output)
 
 country_labs = pd.read_csv('./additional_data/countries_labels.csv')
-country_labs.columns = ['Countries', 'Region', 'Lat', 'Lon']
+country_labs.columns = ['Countries', 'Region', 'Lat', 'Lon', 'Subregion']
 
-confirmed = pd.read_csv("./csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-confirmed_long = ts_record_parse(confirmed, 'Confirmed')
+country_pop = pd.read_csv('./additional_data/WPP2019_TotalPopulationBySex.csv')
+country_pop = country_pop[['Location', 'Time', 'PopTotal']].loc[country_pop.Time == 2020].drop_duplicates()
+country_pop['PopTotal'] = country_pop.PopTotal.apply(lambda x: x * 1000)
+
+
+confirmed_new = pd.read_csv("./csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+confirmed_long = ts_record_parse(confirmed_new, 'Confirmed')
 confirmed_long["Country"] = confirmed_long.Country.apply(lambda x: x.replace("*", "")) # To remove the * from Taiwan
 
 # Putting the tooltip label in for the mapdeck mouseover visualization in the Shiny app`
@@ -89,10 +133,6 @@ deaths_long["Country"] = deaths_long.Country.apply(lambda x: x.replace("*", ""))
 
 # Putting the tooltip label in for the mapdeck mouseover visualization in the Shiny app`
 deaths_long['tooltip_deaths'] = deaths_long.apply(lambda x : x.Country + ": " + str(x.Deaths), axis=1)
-
-
-# recovered = pd.read_csv("./csse_covid_19_data/csse_covid_19_time_series/time_series_19-recovered_global.csv")
-# recovered_long = ts_record_parse(recovered, 'Recovered')
 
 
 # Merging the timeseries data by day and location for COVID-19 confirmed cases, deaths, and recovered cases.
@@ -109,10 +149,7 @@ all_countries.to_feather("./parsed_data/all_countries_ts.feather")
 # SUMMARIZING BY Date
 # Summing all of the confirmed cases, deaths, and recovered patients for each day
 data_totTime_confirmed = all_countries.groupby("Date")['Confirmed'].sum().reset_index()
-
-
 data_totTime_deaths = all_countries.groupby("Date")['Deaths'].sum().reset_index()
-
 
 # data_totTime_recovered = all_countries.groupby("Date")['Recovered'].sum().reset_index()
 
@@ -149,7 +186,7 @@ def normalize_measure(num1, num2):
     return norm_val
 
 #Renaming the country names to align with the table it will be merged with
-country_pop.replace({'China, Taiwan Province of China': 'Taiwan', 
+country_pop.replace({'China, Taiwan Province of China': 'Taiwan',
                      'United States of America': 'US',
                      'Iran (Islamic Republic of)': 'Iran',
                      'Viet Nam': 'Vietnam',
@@ -164,8 +201,11 @@ country_pop.replace({'China, Taiwan Province of China': 'Taiwan',
                      'Democratic Republic of the Congo': 'Congo (Kinshasa)',
                      'Republic of the Congo': 'Congo (Brazzaville)'}, inplace=True)
 
-country_confirmed_newnorm = country_confirmed_new.merge(country_pop[["name", 'pop2020']], left_on="Country", right_on="name").drop(columns=["name"])
-country_confirmed_newnorm["new_confirmed100k"] = country_confirmed_newnorm.apply(lambda x: normalize_measure(x.new_confirmed, x.pop2020), axis=1)
+country_confirmed_newnorm = country_confirmed_new.merge(country_pop[["Location", 'PopTotal']], left_on="Country", right_on="Location").drop(columns=["Location"])
+country_confirmed_newnorm["new_confirmed100k"] = country_confirmed_newnorm.apply(lambda x: normalize_measure(x.new_confirmed, x.PopTotal), axis=1)
+
+country_deaths_newnorm = country_deaths_new.merge(country_pop[["Location", 'PopTotal']], left_on="Country", right_on="Location").drop(columns=["Location"])
+country_deaths_newnorm["new_deaths100k"] = country_deaths_newnorm.apply(lambda x: normalize_measure(x.new_deaths, x.PopTotal), axis=1)
 
 rolling_cases_country = [rolling_ave(country_confirmed_new, country, "new_confirmed") for country in countryList_newCases]
 rolling_cases_country = pd.concat(rolling_cases_country)
